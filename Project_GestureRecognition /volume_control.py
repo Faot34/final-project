2abc507af9aba05cef1812a038430f9016d14c3a
Oludotun Fawole <yemidotun2017@gmail.com>
2025-06-25 11:37:25 +0300

    Update volume_control.py

--- Project_GestureRecognition /volume_control.py
+++ Project_GestureRecognition /volume_control.py
@@ -6,80 +6,100 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-# solution APIs
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
 
-# Volume Control Library Usage 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-volRange = volume.GetVolumeRange()
-minVol , maxVol , volBar, volPer= volRange[0] , volRange[1], 400, 0
+class VolumeGestureController:
+    def __init__(self, cam_width=640, cam_height=480):
+        self.wCam = cam_width
+        self.hCam = cam_height
+        self.cam = cv2.VideoCapture(0)
+        self.cam.set(3, self.wCam)
+        self.cam.set(4, self.hCam)
 
-# Webcam Setup
-wCam, hCam = 640, 480
-cam = cv2.VideoCapture(0)
-cam.set(3,wCam)
-cam.set(4,hCam)
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            model_complexity=0,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_drawing_styles = mp.solutions.drawing_styles
 
-# Mediapipe Hand Landmark Model
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+        # Volume setup
+        self.volume = self._initialize_volume_interface()
+        vol_range = self.volume.GetVolumeRange()
+        self.minVol = vol_range[0]
+        self.maxVol = vol_range[1]
 
-  while cam.isOpened():
-    success, image = cam.read()
+    def _initialize_volume_interface(self):
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        return cast(interface, POINTER(IAudioEndpointVolume))
 
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
+    def _get_landmark_positions(self, hand_landmarks, image):
+        lm_list = []
+        for id, lm in enumerate(hand_landmarks.landmark):
+            h, w, _ = image.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            lm_list.append((id, cx, cy))
+        return lm_list
+
+    def _draw_hand_landmarks(self, image, hand_landmarks):
+        self.mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style()
-            )
+            self.mp_hands.HAND_CONNECTIONS,
+            self.mp_drawing_styles.get_default_hand_landmarks_style(),
+            self.mp_drawing_styles.get_default_hand_connections_style()
+        )
 
-    # multi_hand_landmarks method for Finding position of Hand landmarks
-    lmList = []
-    if results.multi_hand_landmarks:
-      myHand = results.multi_hand_landmarks[0]
-      for id, lm in enumerate(myHand.landmark):
-        h, w, c = image.shape
-        cx, cy = int(lm.x * w), int(lm.y * h)
-        lmList.append([id, cx, cy])          
+    def run(self):
+        while self.cam.isOpened():
+            success, image = self.cam.read()
+            if not success:
+                continue
 
-    # Assigning variables for Thumb and Index finger position
-    if len(lmList) != 0:
-      x1, y1 = lmList[4][1], lmList[4][2]
-      x2, y2 = lmList[8][1], lmList[8][2]
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(image_rgb)
+            image = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
 
-      # Marking Thumb and Index finger
-      cv2.circle(image, (x1,y1),15,(255,255,255))  
-      cv2.circle(image, (x2,y2),15,(255,255,255))   
-      cv2.line(image,(x1,y1),(x2,y2),(0,255,0),3)
-      length = math.hypot(x2-x1,y2-y1)
-      if length < 50:
-        cv2.line(image,(x1,y1),(x2,y2),(0,0,255),3)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    self._draw_hand_landmarks(image, hand_landmarks)
+                    lm_list = self._get_landmark_positions(hand_landmarks, image)
 
-      vol = np.interp(length, [50, 220], [minVol, maxVol])
-      volume.SetMasterVolumeLevel(vol, None)
-      volBar = np.interp(length, [50, 220], [400, 150])
-      volPer = np.interp(length, [50, 220], [0, 100])
+                    if lm_list:
+                        x1, y1 = lm_list[4][1], lm_list[4][2]  # Thumb tip
+                        x2, y2 = lm_list[8][1], lm_list[8][2]  # Index finger tip
 
-      # Volume Bar
-      cv2.rectangle(image, (50, 150), (85, 400), (255, 0, 0), 3)
-      cv2.rectangle(image, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-      cv2.putText(image, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                1, (0, 0, 0), 3)
-    
-    cv2.imshow('handDetector', image) 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-cam.release()
+                        # Draw gesture guide
+                        cv2.circle(image, (x1, y1), 10, (255, 255, 255), cv2.FILLED)
+                        cv2.circle(image, (x2, y2), 10, (255, 255, 255), cv2.FILLED)
+                        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+                        length = math.hypot(x2 - x1, y2 - y1)
+                        if length < 50:
+                            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+                        # Convert length to volume
+                        vol = np.interp(length, [50, 220], [self.minVol, self.maxVol])
+                        vol_bar = np.interp(length, [50, 220], [400, 150])
+                        vol_per = np.interp(length, [50, 220], [0, 100])
+
+                        self.volume.SetMasterVolumeLevel(vol, None)
+
+                        # Draw volume bar and percentage
+                        cv2.rectangle(image, (50, 150), (85, 400), (255, 0, 0), 3)
+                        cv2.rectangle(image, (50, int(vol_bar)), (85, 400), (255, 0, 0), cv2.FILLED)
+                        cv2.putText(image, f'{int(vol_per)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 3)
+
+            cv2.imshow('Volume Control via Gesture', image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cam.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    controller = VolumeGestureController()
+    controller.run()
